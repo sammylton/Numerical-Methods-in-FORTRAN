39ce921bda22592179c0e9fcc8d167f022a0f722
Gagan6164 <32809202+Gagan6164@users.noreply.github.com>
2019-10-02 20:21:58 +0530

    added integration method

--- simpson_int.f
+++ simpson_int.f
@@ -18,5 +18,35 @@
       a=l+f(1)+f(n)
       h=x(2)-x(1)
       print*,((m+a)*h)/3.0,'is the area under the curve'
+          
+
+
+! new method
+	open(9,file='data.dta' )	
+  	do
+         read(9,*,end=10) c,d
+         i=i+1
+        end do
+        
+  10    close(9)
+        print*,i
+	open(9,file='data.dta' )	
+  	do j=1,100000
+         read(9,*) c,d
+         if(j==1 .and. j==i)then
+         sum1=sum1+d
+         else 
+	  sum1=sum1+d*2  
+	 endif       
+        end do
+        close(9)
+       	open(9,file='data.dta' )
+	read(9,*)k
+        read(9,*)l
+        area=(l-k)*sum1/2
+        close(9)
+        print*, i,area
+
+	
       stop
-      end    
+      end
